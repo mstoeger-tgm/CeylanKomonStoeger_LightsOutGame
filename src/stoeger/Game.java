d97738103d3ae3ff9f6fbbcb168a7307e1fc35c6
pamiko <pamiko@outlook.com>
2014-12-10 09:50:52 +0100

    clicked Methode (teilweise) der Game-Klasse

--- src/stoeger/Game.java
+++ src/stoeger/Game.java
@@ -8,11 +8,31 @@ package stoeger;
 public class Game {
 
 	private GUI gui;
-	
+
 	public Game() {
-		
+		gui = new GUI(this);
 	}
 	
+	/**
+	 * Ändert den Zustand eines bestimmten und
+	 * seiner umliegenden Schalter
+	 * (links, rechts, darüber, darunter)
+	 * @param switchIndex
+	 */
+	public void clicked(int switchIndex) {
+		gui.switchState(switchIndex);
+		if(switchIndex%GUI.FIELDS!=0){//wenn der Schalter nicht am linken Ende der Zeile ist
+			gui.switchState(switchIndex-1);
+		}
+		if(switchIndex%GUI.FIELDS!=4){//wenn der Schalter nicht am rechten Ende der Zeile ist
+			gui.switchState(switchIndex+1);
+		}
+		
+		
+		
+		
+	}
+
 	public static void main(String[] args) {
 		new Game();
 	}

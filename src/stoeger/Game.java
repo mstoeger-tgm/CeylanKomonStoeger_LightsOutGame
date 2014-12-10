7e043ade5a53c99d11aec9187364cb94c120b08f
pamiko <pamiko@outlook.com>
2014-12-10 09:25:53 +0100

    Spiellogik (noch leer)

---
+++ src/stoeger/Game.java
@@ -0,0 +1,19 @@
+package stoeger;
+
+/**
+ * 
+ * @author Patrick Komon
+ * @version 10-12-2014
+ */
+public class Game {
+
+	private GUI gui;
+	
+	public Game() {
+		
+	}
+	
+	public static void main(String[] args) {
+		new Game();
+	}
+}
